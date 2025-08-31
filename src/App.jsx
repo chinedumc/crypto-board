56806009307097172d562480a2e97f160cd42da6
chinedumc <chibokachinedum@gmail.com>
2025-08-31 18:12:37 +0100

    Created CoinCard component and a limit feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
-const API_URL =
-	"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-
+import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
+const API_URL = import.meta.env.VITE_API_URL;
 const App = () => {
 	const [coins, setCoins] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	const [limit, setLimit] = useState(10);
 
 	useEffect(() => {
 		const fetchCoins = async () => {
 			try {
-				const res = await fetch(API_URL);
+				const res = await fetch(
+					`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+				);
 				if (!res.ok) throw new Error("Failed to fetch data");
 				const data = await res.json();
 				console.log(data);
@@ -39,9 +42,25 @@ const App = () => {
 		// 		setError(err.message);
 		// 		setLoading(false);
 		// 	});
-	}, []);
+	}, [limit]);
 
-	return <h1>Crypto Board</h1>;
+	return (
+		<>
+			<h1>Crypto Board</h1>
+			{loading && <p>Loading...</p>}
+			{error && <div className="error">{error}</div>}
+
+			<LimitSelector onLimitChange={setLimit} limit={limit} />
+
+			{!loading && !error && (
+				<main className="grid">
+					{coins.map((coin) => (
+						<CoinCard key={coin.id} coin={coin} />
+					))}
+				</main>
+			)}
+		</>
+	);
 };
 
 export default App;
