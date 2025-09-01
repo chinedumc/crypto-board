02b116d0f9806917eb3e155154f04d18bc2f5048
chinedumc <chibokachinedum@gmail.com>
2025-09-01 23:29:31 +0100

    Implemented filtering and sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 const API_URL = import.meta.env.VITE_API_URL;
 const App = () => {
 	const [coins, setCoins] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 	const [limit, setLimit] = useState(10);
+	const [filter, setFilter] = useState("");
+	const [sortBy, setSortBy] = useState("market_cap_desc");
 
 	useEffect(() => {
 		const fetchCoins = async () => {
@@ -44,19 +48,49 @@ const App = () => {
 		// 	});
 	}, [limit]);
 
+	const filteredCoins = coins
+		.filter((coin) => {
+			return (
+				coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+				coin.symbol.toLowerCase().includes(filter.toLowerCase())
+			);
+		})
+		.slice()
+		.sort((a, b) => {
+			switch (sortBy) {
+				case "market_cap_desc":
+					return b.market_cap - a.market_cap;
+				case "market_cap_asc":
+					return a.market_cap - b.market_cap;
+				case "price_desc":
+					return b.current_price - a.current_price;
+				case "price_asc":
+					return a.current_price - b.current_price;
+				case "change_desc":
+					return a.price_change_percentage_24h - b.price_change_percentage_24h;
+				case "change_asc":
+					return b.price_change_percentage_24h - a.price_change_percentage_24h;
+			}
+		});
+
 	return (
 		<>
 			<h1>Crypto Board</h1>
 			{loading && <p>Loading...</p>}
 			{error && <div className="error">{error}</div>}
-
-			<LimitSelector onLimitChange={setLimit} limit={limit} />
+			<div className="top-controls">
+				<FilterInput filter={filter} onFilterChange={setFilter} />
+				<LimitSelector onLimitChange={setLimit} limit={limit} />
+				<SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+			</div>
 
 			{!loading && !error && (
 				<main className="grid">
-					{coins.map((coin) => (
-						<CoinCard key={coin.id} coin={coin} />
-					))}
+					{filteredCoins.length > 0 ? (
+						filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+					) : (
+						<p>No Matching Coins</p>
+					)}
 				</main>
 			)}
 		</>
